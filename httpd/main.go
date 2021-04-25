cb3d721f30bb08a08749a612e00734610d5648cf
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-25 21:56:02 +0530

    (feat): add testing

--- httpd/main.go
+++ httpd/main.go
@@ -1,17 +1,19 @@
 package main
 
-import (
-	"Go-REST-API/httpd/handler"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
+	// r := gin.Default()
 
-	// test route /ping -> pong
-	r.GET("/ping", handler.PingGet())
+	// // test route /ping -> pong
+	// r.GET("/ping", handler.PingGet())
 
-	// boot up router
-	r.Run()
+	// // boot up router
+	// r.Run()
+
+	// feed := newsfeed.New()
+
+	// fmt.Println(feed)
+
+	// feed.Add(newsfeed.Item{"Hello", "How ya doing!"})
+
+	// fmt.Println(feed)
 }
